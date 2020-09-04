7287fddeebdce867442558b29d7703e04aa4a9c2
nelson-singleton <nsingleton@ymail.com>
2020-09-04 10:30:34 -0500

    Finished PlantList component

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -12,6 +12,15 @@ constructor(){
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  componentDidMount(){
+    axios
+      .get ("http://localhost:3333/plants")
+      .then (res =>
+        this.setState ({
+          plants: [res.data] 
+        }) )
+      .catch (err => console.log ("Error fetching plants."))
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
