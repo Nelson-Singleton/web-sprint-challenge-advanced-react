0aafdd7c603d17e5bbe8aaa9d20f694d325a9a28
nelson-singleton <nsingleton@ymail.com>
2020-09-04 13:10:25 -0500

    Finished second test. MVP met

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { render, getByText, getByTestId, queryByText} from "@testing-library/react";
+import { render, screen, getByText, getByTestId, queryByText, fireEvent} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
 test("form header renders", () => {
+    
     //renders component to vdom.. Arrange
     const { getByText } = render(<CheckoutForm />)
     //or queryByText
@@ -22,12 +23,23 @@ test("form header renders", () => {
 
 test("form shows success message on submit with form details", () => {
     render(<CheckoutForm />)
-    const valueFirstName
-    const valueLastName
-    const valueAddress
-    const valueCity
-    const valueState
-    const valueZip
+
+    //form submits successfully without any values entered, so we'll just get the name fields.
+    const valueFirstName = screen.getByLabelText(/First Name/i)
+    const valueLastName = screen.getByLabelText(/Last Name/i)
+
+    //find text fields by label name, and enter data
+    fireEvent.change(valueFirstName, {target: {value: "Nelson"}})
+    fireEvent.change(valueLastName, {target: {value: "Singleton"}})
+
+    //find submit button
+    const submit = screen.getByRole('button', {name: /Checkout/i})
     
+    //click submit
+    fireEvent.click(submit)
+
+    //find confirmation message by submitted name message
+    const findMyName = screen.getByText(/Nelson Singleton/i)
+    expect(findMyName).toBeInTheDocument()    
     
 });
